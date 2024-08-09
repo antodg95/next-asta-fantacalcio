47e5a3ef2ec39462aec6c2dbf1aac2e0c05a36b9
Antonio Di Giulio <antodg95@gmail.com>
2024-08-09 17:57:09 +0200

    Disable some ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -57,11 +57,11 @@ const config = {
       }
     ],
     //Disable ESLint for now.
-    "@typescript-eslint/no-unsafe-assignment": "warn",
-    "@typescript-eslint/no-explicit-any" : "warn",
-    "@typescript-eslint/no-unsafe-member-access" : "warn",
-    "@typescript-eslint/no-unsafe-argument" : "warn",
-    "@typescript-eslint/no-unsafe-call" : "warn"
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-explicit-any" : "off",
+    "@typescript-eslint/no-unsafe-member-access" : "off",
+    "@typescript-eslint/no-unsafe-argument" : "off",
+    "@typescript-eslint/no-unsafe-call" : "off"
   }
 }
 module.exports = config;
